64cbed6dee7edd502bab6dea1fe0cb3baca76967
deepakkroxford <deepakkr.oxford@gmail.com>
2025-05-01 00:11:42 +0530

    adding the routes

---
+++ Ui/src/pages/ResetPassword.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div>
+      <h1>Reset Password Page</h1>
+      {/* Additional content can be added here */}
+    </div>
+  )
+}
+
+export default ResetPassword
+
+
