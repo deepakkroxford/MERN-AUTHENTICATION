bc70c2e38dfd0d1b8739521eb3d4595eda487bab
deepakkroxford <deepakkr.oxford@gmail.com>
2025-05-01 12:24:35 +0530

    solving corss site problem

--- Server/app.js
+++ Server/app.js
@@ -4,24 +4,27 @@ import cookieParser from 'cookie-parser';
 import dbConnection from './config/databaseConnection.js';
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
+import cors from 'cors'
 
 configDotenv();
 dbConnection();
 
+const allowedOrigin = ['http://localhost:5173']
+
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({}));
 app.use(cookieParser());
-//app.use(cors({credentials:true}));
+app.use(cors({ origin: allowedOrigin, credentials: true }));
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('api is working perfectly');
 })
 
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4040;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`serve is running on port http://localhost:${PORT}`);
 })
