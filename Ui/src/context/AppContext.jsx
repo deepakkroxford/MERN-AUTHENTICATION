15bc917f18cc571af9a786fd487d8b6b55f28207
deepakkroxford <deepakkr.oxford@gmail.com>
2025-07-23 21:08:14 +0530

    fixing the appcontex.jsx and .env

--- Ui/src/context/AppContext.jsx
+++ Ui/src/context/AppContext.jsx
@@ -12,7 +12,7 @@ export const AppContextProvider = (props) => {
     const getAuthState = async () => {
         try {
             axios.defaults.withCredentials = true;
-            const { data } = await axios.get('http://localhost:4040/api/auth/is-auth');
+            const { data } = await axios.get(backendUrl + '/api/auth/is-auth',{ withCredentials: true });
             console.log(data);
             if (data.success) {
                 setIsLoggedin(true);
